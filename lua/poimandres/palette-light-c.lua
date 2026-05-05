61c7ed5a118f470b02c07a566823892aaac283d9
Tevz Basaj <tevz.basaj@gmail.com>
2026-05-05 08:37:12 +0200

    2 new variant, b and c

---
+++ lua/poimandres/palette-light-c.lua
@@ -0,0 +1,27 @@
+-- BOXUK
+local gr = '#019d76'
+local bl = '#017c9d'
+local te = '#15b8ae'
+return {
+  yellow = '#d3ba00',
+  teal1 = te,
+  teal2 = te,
+  teal3 = te,
+  blue1 = bl,
+  blue2 = bl,
+  blue3 = bl,
+  blue4 = bl,
+  pink1 = '#A84A8A',
+  pink2 = '#C061A3',
+  pink3 = '#8C3A6D',
+  blueGray1 = gr,
+  blueGray2 = gr,
+  blueGray3 = gr,
+  background1 = '#FFFFFF',
+  background2 = '#F4F7FB',
+  background3 = '#E9EDF5',
+  background4 = '#DDE3EE',
+  text = '#1F2430',
+  white = '#FFFFFF',
+  none = 'NONE',
+}
